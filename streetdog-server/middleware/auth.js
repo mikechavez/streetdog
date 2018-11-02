429c3cfcfb1a4b2dab8e46b51efc042d8e05e901
mikechavez <mchavez3@uw.edu>
2018-11-02 13:56:18 -0700

    add authorization handler

--- streetdog-server/middleware/auth.js
+++ streetdog-server/middleware/auth.js
@@ -1,11 +1,12 @@
 require('dotenv').load();
 const jwt = require('jsonwebtoken');
 
+// AUTHENTICATION OF USER
 exports.loginRequired = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.SECRET_KEY, function(err, payload) {
-      if(payload) {
+      if (payload) {
         return next();
       } else {
         return next({
@@ -19,5 +20,27 @@ exports.loginRequired = function(req, res, next) {
       status: 401,
       message: 'Please log in first, broski'
     })
+  }
+}
+
+// AUTHORIZATION OF USER
+exports.ensureCorrectUser = function(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_KEY, function(err, payload) {
+      if (payload && payload.id === req.params.id) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: 'Unauthorized, duder'
+        });
+      }
+    })
+  } catch (err) {
+    return next({
+      status: 401,
+      message: 'Unauthorized, broski'
+    });
   }
 }
