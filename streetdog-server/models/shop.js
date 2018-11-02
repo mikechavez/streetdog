15765d26b6e344429345a2e0460a9a3d1cc0e805
mikechavez <mchavez3@uw.edu>
2018-11-02 14:37:58 -0700

    Add user / shop references

--- streetdog-server/models/shop.js
+++ streetdog-server/models/shop.js
@@ -21,6 +21,10 @@ const shopSchema = new mongoose.Schema({
   description: {
     type: String,
     require: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }
 });
 
--- streetdog-server/models/user.js
+++ streetdog-server/models/user.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
   profileImageUrl: {
     type: String,
     // required: true
+  },
+  shop: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Shop'
   }
 });
 
